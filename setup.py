5e9ba9a7543dc427d6e59f917486e3dbe7e4f86c
Masamitsu MURASE <masamitsu.murase@gmail.com>
2021-05-08 02:04:17 +0900

    Release Version 1.3.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='msal_interactive_token_acquirer',
-    version='1.2.0',
+    version='1.3.0',
     description='A library to get MSAL token interactively for native client.',
     long_description=long_description,
     long_description_content_type='text/markdown',
